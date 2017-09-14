c90f8d90284df0410910dedcdfcca3653d2067e4
joaomatos28 <joaosoaresmatos28@gmail.com>
2017-09-14 17:08:35 -0300

    Funcionalidade de fugir

--- core/src/br/cefetmg/games/movement/behavior/Fugir.java
+++ core/src/br/cefetmg/games/movement/behavior/Fugir.java
@@ -4,6 +4,8 @@ import br.cefetmg.games.movement.AlgoritmoMovimentacao;
 import br.cefetmg.games.movement.Direcionamento;
 import br.cefetmg.games.movement.Pose;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.Vector3;
+import static java.lang.Math.random;
 
 /**
  * Guia o agente de forma a fugir na direção contrária ao alvo.
@@ -16,18 +18,22 @@ public class Fugir extends AlgoritmoMovimentacao {
 
     public Fugir(float maxVelocidade) {
         super(NOME);
+        this.maxVelocidade = maxVelocidade;
     }
 
     @Override
     public Direcionamento guiar(Pose agente) {
         Direcionamento output = new Direcionamento();
-
-        // calcula que direção tomar (configura um objeto Direcionamento 
-        // e o retorna)
-        // ...
-        // super.alvo já contém a posição do alvo
-        // agente (parâmetro) é a pose do agente que estamos guiando
-        // ...
+        
+        output.velocidade = new Vector3(agente.posicao.x - super.alvo.getObjetivo().x,
+                                        agente.posicao.y - super.alvo.getObjetivo().y,
+                                        agente.posicao.z - super.alvo.getObjetivo().z);
+        output.velocidade.nor().scl(maxVelocidade);
+        agente.olharNaDirecaoDaVelocidade(output.velocidade);
+        
+        
+        
+        
         return output;
     }
 
