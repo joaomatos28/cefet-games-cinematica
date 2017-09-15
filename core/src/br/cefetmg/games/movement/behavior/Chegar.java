fb18d331753398ea354ad5e2c00daa08e828070f
joaomatos28 <joaosoaresmatos28@gmail.com>
2017-09-14 22:14:54 -0300

    Mesmo do chegar

---
+++ core/src/br/cefetmg/games/movement/behavior/Chegar.java
@@ -0,0 +1,51 @@
+package br.cefetmg.games.movement.behavior;
+
+import br.cefetmg.games.movement.AlgoritmoMovimentacao;
+import br.cefetmg.games.movement.Direcionamento;
+import br.cefetmg.games.movement.Pose;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Guia o agente na direção do alvo.
+ *
+ * @author Flávio Coutinho <fegemo@cefetmg.br>
+ */
+public class Buscar extends AlgoritmoMovimentacao {
+
+    private static final char NOME = 's';
+
+    public Buscar(float maxVelocidade) {
+        this(NOME, maxVelocidade);
+    }
+
+    protected Buscar(char nome, float maxVelocidade) {
+        super(nome);
+        this.maxVelocidade = maxVelocidade;
+    }
+
+    @Override
+    public Direcionamento guiar(Pose agente) {
+        Direcionamento output = new Direcionamento();
+        
+        output.velocidade = new Vector3(super.alvo.getObjetivo().x - agente.posicao.x,
+                                        super.alvo.getObjetivo().y - agente.posicao.y,
+                                        super.alvo.getObjetivo().z - agente.posicao.z);
+        output.velocidade.nor().scl(maxVelocidade);
+        agente.olharNaDirecaoDaVelocidade(output.velocidade);
+        
+        
+        // calcula que direção tomar (configura um objeto Direcionamento 
+        // e o retorna)
+        // ...
+        // super.alvo já contém a posição do alvo
+        // agente (parâmetro) é a pose do agente que estamos guiando
+        // ...
+        return output;
+    }
+
+    @Override
+    public int getTeclaParaAtivacao() {
+        return Keys.S;
+    }
+}
