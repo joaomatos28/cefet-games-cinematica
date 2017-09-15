f00a6a8919c783fc087bf3d9879ab7a748dcb4c8
joaomatos28 <joaosoaresmatos28@gmail.com>
2017-09-14 22:46:38 -0300

    Add chegar

--- core/src/br/cefetmg/games/Cinematica.java
+++ core/src/br/cefetmg/games/Cinematica.java
@@ -44,6 +44,7 @@ public class Cinematica extends ApplicationAdapter {
     private Buscar buscar;
     private Vagar vagar;
     private Fugir fugir;
+    private Chegar chegar;
     private AlgoritmoMovimentacao algoritmoCorrente;
     private Array<AlgoritmoMovimentacao> algoritmos;
     private String stringAlgoritmoCorrente;
@@ -71,10 +72,13 @@ public class Cinematica extends ApplicationAdapter {
         buscar.alvo = objetivo;
         vagar = new Vagar(40, 2);
         fugir = new Fugir(40);
+        chegar = new Chegar(40, 5, 1);
         fugir.alvo = buscar.alvo;
+        chegar.alvo = buscar.alvo;
         algoritmos.add(buscar);
         algoritmos.add(vagar);
         algoritmos.add(fugir);
+        algoritmos.add(chegar);
         algoritmoCorrente = vagar;
         stringAlgoritmoCorrente = "Algoritmo corrente: "
                 + algoritmoCorrente.getNome();
